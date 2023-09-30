cc9ada766390c6b73e808cd8b4a17175d8ec27da
serg310583 <serg310583@mail.ru>
2023-09-30 12:34:55 +0700

    remoove JornalItem from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
 import { useState } from 'react';
 import './App.css';
-import CardButton from './components/CardButton/CardButton';
 import Header from './components/Header/Header';
 import JornalAddButton from './components/JornalAddButton/JornalAddButton';
 import JornalList from './components/JornalList/JornalList';
-import JournalItem from './components/JournalItem/JournalItem';
 import Body from './components/layouts/Body/Body';
 import LeftPanel from './components/layouts/LeftPanel/LeftPanel';
 import JornalForm from './components/JornalForm/JornalForm';
 
 const INITIAL_DATA = [
-	{
-		id: 1,
-		title: 'Подготовка к обновлению курсов',
-		text: 'Горные походы открывают удивительные ландшафты',
-		date: new Date()
-	},
-	{
-		id: 2,
-		title: 'Поход в годы',
-		text: 'Думал, что очень много времени...',
-		date: new Date()
-	}
+	// {
+	// 	id: 1,
+	// 	title: 'Подготовка к обновлению курсов',
+	// 	text: 'Горные походы открывают удивительные ландшафты',
+	// 	date: new Date()
+	// },
+	// {
+	// 	id: 2,
+	// 	title: 'Поход в годы',
+	// 	text: 'Думал, что очень много времени...',
+	// 	date: new Date()
+	// }
 ];
 function App() {
 	const [items, setItems] = useState(INITIAL_DATA);
@@ -30,17 +28,11 @@ function App() {
 			title: item.title,
 			text: item.text,
 			date: new Date(item.date),
-			id: Math.max(...oldItems.map(i => i.id)) + 1
+			id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1
 		}]);
 	};
 	
-	const sortItem = (a, b) =>{
-		if (a.date < b.date){
-			return 1;
-		} else {
-			return -1;
-		}
-	};
+	
 
 	return (
 		<div className='app'>
@@ -49,17 +41,8 @@ function App() {
 
 				<JornalAddButton/>
 
-				<JornalList>
-					{items.sort(sortItem).map(el => (
-						<CardButton key={el.id}>
-							<JournalItem
-								title={el.title}
-								text={el.text}
-								date={el.date}
-							/>
-						</CardButton>
-					))}					
-				</JornalList>
+				<JornalList items = {items}/>
+					
 				
 			</LeftPanel>
 			<Body>
--- src/components/JornalList/JornalList.jsx
+++ src/components/JornalList/JornalList.jsx
@@ -1,12 +1,34 @@
 import './JornalList.css';
+import CardButton from '../CardButton/CardButton';
+import JournalItem from '../JournalItem/JournalItem';
 
 
-function JornalList({children}) {
-	return ( 
-		<div className='jornal-list'>
-			{children}
-		</div>
-	);
+
+
+function JornalList({ items }) {
+	if (items.length === 0){
+		return <p>Создайте первую запись</p>;
+	}
+
+	const sortItem = (a, b) =>{
+		if (a.date < b.date){
+			return 1;
+		} else {
+			return -1;
+		}
+	};
+
+	return <>
+		{items.sort(sortItem).map(el => (
+			<CardButton key={el.id}>
+				<JournalItem
+					title={el.title}
+					text={el.text}
+					date={el.date}
+				/>
+			</CardButton>
+		))}
+	</>;	
 }
   
 export default JornalList;
