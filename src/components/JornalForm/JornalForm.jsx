b71eb1519297d30183edb38a99dc74c9b9489e54
serg310583 <serg310583@mail.ru>
2023-10-04 22:44:11 +0700

    installed classnames

--- src/components/JornalForm/JornalForm.jsx
+++ src/components/JornalForm/JornalForm.jsx
@@ -1,6 +1,7 @@
 import styles from './JornalForm.module.css';
 import Button from '../Button/Button';
 import { useState } from 'react';
+import cn from 'classnames';
 
 
 function JornalForm({ onSubmit }) {
@@ -40,10 +41,16 @@ function JornalForm({ onSubmit }) {
 	return ( 
 		
 		<form className={styles['jornal-form']} onSubmit={addJornalItem}>
-			<input type="text" name='title'className={`${styles['input']} ${formValidState.title ? '' : styles['invalid']}`}/>
-			<input type="date" name='date' className={`${styles['input']} ${formValidState.date ? '' : styles['invalid']}`}/>
+			<input type="text" name='title'className={cn(styles['input'], {
+				[styles['invalid']]:!formValidState.title
+			})}/>
+			<input type="date" name='date' className={cn(styles['input'], {
+				[styles['invalid']]:!formValidState.date
+			})}/>
 			<input type="text" name='tag' />
-			<textarea name="post" id="" cols="30" rows="10" className={`${styles['input']} ${formValidState.post ? '' : styles['invalid']}`}></textarea>
+			<textarea name="post" id="" cols="30" rows="10" className={cn(styles['input'], {
+				[styles['invalid']]:!formValidState.post
+			})}></textarea>
 			<Button text="Сохранить"/>
 		</form>		
 	);
